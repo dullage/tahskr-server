9ed854eb72fa33c49dae65973ab722fd6999d48c
Adam Dullage <adam@dullage.com>
2020-04-25 14:28:13 +0100

    Dev Deployment

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,11 +4,11 @@ set -ev
 
 VERSION=$(cat version.json | jq -r ".version")
 ARCH=$1
-DEPLOY=$2
+PUSH=$2
 
 docker build -t $DOCKER_REPO_SLUG:$VERSION-$ARCH $TRAVIS_BUILD_DIR
 
-if [[ $DEPLOY == deploy ]]
+if [[ $PUSH == push ]]
 then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker push $DOCKER_REPO_SLUG:$VERSION-$ARCH
